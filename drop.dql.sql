a1f37ed2292849768483fef9e91b1cc732d90281
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2026-05-23 18:00:18 +0530

    initial commit

---
+++ drop.dql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE makeup(
+	brand VARCHAR(255),
+	product VARCHAR(255),
+	year INT
+);
+
+--DROP TABLE is used to drop an existing table in a database
+DROP TABLE makeup;
+
+SELECT * FROM makeup;
