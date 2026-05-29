5625ebc34707567a04661469409f5c09dc6f0472
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2026-05-29 19:31:53 +0530

    count function

---
+++ CountFunction.sql
@@ -0,0 +1,32 @@
+CREATE TABLE orderData (
+	o_id INT,
+	o_name VARCHAR,
+	o_city VARCHAR
+);
+
+INSERT INTO orderData(o_id,o_name,o_city)
+VALUES  (1 , 'habiba','landon'),
+		(2 , 'Muskan','Mumbai'),
+		(3 , 'sima','Uk'),
+		(4 , 'Anam','japan'),
+		(5 , 'Ruhina','england'),
+		(6 , 'Rubi','Mumbai'),
+		(7 , 'Shiba','landon'),
+		(8 , 'Hina','korea'),
+		(9 , 'Shabana','landon'),
+		(10 , 'Aasiya','landon'),
+		(11 , 'hiba','korea'),
+		(12 , 'rehana','pakistan'),
+		(13 , 'habiba','UK'),
+		(14 , 'Azleen','landon'),
+		(15 , 'rumi','UK'),
+		(16 , 'sheza','landon'),
+		(17 , 'mayra','landon'),
+		(18 , 'hida','landon'),
+		(19 , 'nida','landon'),
+		(20 , 'laiba','landon');
+
+-- COUNT() Function
+SELECT COUNT(o_id) FROM orderData;
+--WHERE CLAUSE
+SELECT COUNT(o_name) FROM orderData WHERE o_name = 'habiba' ;
