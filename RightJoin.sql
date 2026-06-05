48b4c09311d3e46b255e0701816c3894d65ca1fc
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2026-06-05 16:23:11 +0530

    Keep right

---
+++ RightJoin.sql
@@ -0,0 +1,41 @@
+CREATE TABLE testproduct(
+testproduct_id INT,
+product_name VARCHAR(255),
+category_id INT
+);
+
+INSERT INTO testproduct (testproduct_id,product_name,category_id)
+VALUES  (1,'Fruit Cake',3),
+		(2,'Healthy Mix',8),
+		(3,'Secret Recipe',9),
+		(4,'Best Apples',6),
+		(5,'Favorite Cheese',4),
+		(6,'Home Made Pizza',5),
+		(7,'Special Salmon',2),
+		(8,'Rich Spaghetti',1),
+		(9,'Ice-cream Palor',18),
+		(10,'Scary Stuff',11);
+SELECT * FROM testproduct;
+
+CREATE TABLE categories (
+category_id INT,
+category_name VARCHAR(255),
+description VARCHAR (255)
+);
+
+INSERT INTO categories (category_id,category_name,description)
+VALUES  (1,'Beverages','soft drink ,coffees, teas, beers and ales'),
+		(2,'Condiments','Sweet and savory sauces,spreads and seasonings'),
+		(3,'Confection','Desserts, candies, sweet breads'),
+		(4,'Dariy Product','Cheeses'),
+		(5,'Grains/Cereals','Breads, crackers, pasta, cereal'),
+		(6,'Meat/Poulty','Prepared meats'),
+		(7,'Produce','Dried fruit and bean curd'),
+		(8,'Seafood','seawees and fish');
+SElECT * FROM categories ;   
+
+--RIGHT JOIN / RIGHT OUTER JOIN
+--Return all records from the right table and the matched records from the left table. 
+--If there is no match it return the null 
+SELECT testproduct_id,product_name,category_name FROM testproduct RIGHT JOIN categories
+ON testproduct.category_id = categories.category_id;
